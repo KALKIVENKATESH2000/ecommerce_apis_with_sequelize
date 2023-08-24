29579bf0c656f1c67c5d084ff64384d9b39c38b3
KALKIVENKATESH2000 <kalkivenkateshdonga@gmail.com>
2023-08-24 14:32:24 +0530

    'new'

--- admin/admin/settings.py
+++ admin/admin/settings.py
@@ -26,12 +26,13 @@ SECRET_KEY = 'django-insecure-&0!ow_6%$3#bnb8om8297-grfote@^ubp$2la3je**ab+yy&%q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'adminlteui',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -142,3 +143,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+ADMINLTE_SETTINGS = {
+    'copyright': 'John Smith',
+}
